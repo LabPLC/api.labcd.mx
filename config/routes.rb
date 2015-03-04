d4a9d5000814666cdeb1f5b0d4f89c8871cdade7
abigail sosa <abigail.sosa.h@gmail.com>
2015-03-04 13:48:17 -0600

    Se implementa api finanzas para consultar pagos

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :semovi_taxis
   namespace :finanzas do
     resources :linea_captura
+    resources :pagos, only: [] do
+      get :consulta, on: :collection
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
