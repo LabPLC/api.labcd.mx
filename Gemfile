418a30c33c5432803f677868d36ca1edbaabd76e
mikesaurio <mikesaurio@gmail.com>
2015-03-05 14:15:39 -0600

    soap regresa 303

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ group :development, :test do
   gem "factory_girl_rails"
   gem 'spring'
   gem "better_errors"
+  gem  "binding_of_caller"
 end
 
 group :test do
--- app/controllers/alerta_amber_controller.rb
+++ app/controllers/alerta_amber_controller.rb
@@ -1,9 +1,27 @@
-class AlertaAmberController < ApplicationController
- @@wsdl='http://172.19.80.208/ws/alertaamber.asmx?wsdl'
-
-  def index
 require 'savon'
-client =Savon.client(wsdl: @@wsdl, log_level: :error, log: false)
-puts client.operations
-  end
+class AlertaAmberController < ApplicationController
+ @@wsdl = 'http://187.217.178.196/wsalertaamber.asmx?wsdl'
+    @@cliente = Burocracia::WS.new(@@wsdl) do |client|
+      client.default_params = {
+          tr: '1'
+      }
+
+      client.default_action = :solicitar_validez
+      client.default_response = :solicitar_validez_response
+
+      client.response_parser = -> (data) {
+        data[:respuesta]
+      }
+end
+ def index
+    
+      begin
+        response = @@cliente.call({'tr' => 1})
+      rescue Exception => e
+        return render status: 500, json: {error: e.message}
+      end
+
+      render json: response
+end
+
 end
