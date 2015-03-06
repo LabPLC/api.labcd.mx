cb5818b7b6603d94d8e705af1fc890296f782446
Raúl Jiménez <jimenezjrs@gmail.com>
2015-03-05 21:37:09 -0600

    added a test for taxis and a respective helper

---
+++ spec/requests/taxis_spec.rb
@@ -0,0 +1,23 @@
+# semovi_taxis/A05601
+
+require 'rails_helper'
+
+describe 'Vehicles' do
+  describe 'GET show' do
+    it 'returns the vehicle information' do
+      get '/semovi_taxis/A05601'
+      taxi = JSON.parse(response.body)
+
+      expect(response_endpoint_keys_in(body: taxi, children: [
+        "id",
+        "code",
+        "placa",
+        "marca_modelo",
+        "status",
+        "fecha",
+        "created_at",
+        "updated_at"
+      ])).to be
+    end
+  end
+end
