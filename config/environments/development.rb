35bd45c2dfb938f12b8046d8dc531fc637398644
Raúl Jiménez <jimenezjrs@gmail.com>
2015-03-07 15:35:01 -0600

    added redis to the development environment config file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+require "redis"
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -38,4 +40,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Setting up redis using the default configuration
+  REDIS = Redis.new
 end
