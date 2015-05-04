bea4c2c478c2391693e0cea9df637441bef6b461
Carlos Chacon <carlos.chacon@gmail.com>
2015-05-04 10:25:00 -0500

    Permite placas de otros estados, placas nuevas (2015) y de taxi

--- app/controllers/v1/seguridad/corralones_controller.rb
+++ app/controllers/v1/seguridad/corralones_controller.rb
@@ -8,7 +8,7 @@ module V1
 
       def show
         placa = params[:id].upcase
-          exp_placa = '^(\d{3}[A-Z]{3})$'
+          exp_placa = '^((\d{3}[a-zA-Z]{3})|([a-zA-Z]{1}\d{2}[a-zA-Z]{3})|([a-zA-Z]{3}\d{3,4})|([a-zA-Z]{1}\d{5}))$'
         if placa.match(exp_placa)
           render json:  Corralones.call_service("http://201.144.220.174/mpws/index.php/corralon/buscar_en_corralon", placa)
         else
